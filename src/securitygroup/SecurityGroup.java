edca47f594ae467d715532a64b753658e175d64c
Franklin Camacho <camachof44@gmail.com>
2022-11-17 16:02:06 -0400

    Update

---
+++ src/securitygroup/SecurityGroup.java
@@ -0,0 +1,17 @@
+
+package securitygroup;
+
+/**
+ *
+ * @author Mundo Telefonico
+ */
+public class SecurityGroup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
